450e8873360463bd37ce996f861c27bf64c9ed05
Osooluwa <osooluwaajibola@gmail.com>
2024-01-29 13:53:44 -0800

    Task 0

---
+++ task0.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _printf(const char *format, ...): format of expected output
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+	int x;
+
+	x = 2 + 2;
+
+	printf("The sum of x is : %d\n, x");
+
+	return 0;
+}
