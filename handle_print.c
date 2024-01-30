b0af07087754253b62406116b79794c532caeb71
Osooluwa <osooluwaajibola@gmail.com>
2024-01-30 13:57:07 -0800

    Updated

--- handle_print.c
+++ handle_print.c
@@ -29,7 +29,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	
 	if (fmt_types[i].fmt == '\0')
 	{
-		if (fmt_types[i].fmt == '\0')
+		if (fmt[*ind] == '\0')
 			return (-1);
 		unknow_len += write(1, "%%", 1);
 		if (fmt[*ind - 1] == ' ')
@@ -46,5 +46,5 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		unknow_len += write(1, &fmt[*ind], 1);
 		return (unknow_len);
 	}
-	return (unknow_len);
+	return (printed_chars)
 }
